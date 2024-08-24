674c37cb43278f7d8c6125e04bec00786d7e6376
cossgii <eastar385@naver.com>
2024-08-24 22:52:50 +0900

    Feat:카드와인리스트구현수정

--- src/types/Card.tsx
+++ src/types/Card.tsx
@@ -1,9 +1,6 @@
 import imageProp from '@/types/Image';
-<<<<<<< HEAD
+
 import { recentReview } from './ReviewProps';
-=======
-import { recentReview, reviewDetailType } from './ReviewProps';
->>>>>>> 78b8854da01b18a14029bb66d6abbfb49e7546d0
 
 export interface CardProps {
   image: imageProp;
@@ -11,11 +8,9 @@ export interface CardProps {
   wineDesc: string;
   winePrice: number;
   childeren?: React.ReactNode;
-<<<<<<< HEAD
+
   review?: recentReview | null;
-=======
-  review?: recentReview | string;
->>>>>>> 78b8854da01b18a14029bb66d6abbfb49e7546d0
+
   avgRating?: number | null;
   reviewCount?: number;
   recentReview?: recentReview | null;
